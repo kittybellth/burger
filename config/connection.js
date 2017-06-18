f64bb072c1163b8cfa48df01e6cd6842b541b3b8
kittybellth <kittybell.th@gmail.com>
2017-06-17 23:28:30 -0500

    fixed SQL connection with JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
+var connection;
 // create connection with mysql
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_UBL){
+  connection = mysql.createConnection(process.env.JAWSDB_UBL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
