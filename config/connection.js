083bec14841357ca10e63ff76f23cd7afadc6b95
kittybellth <kittybell.th@gmail.com>
2017-06-17 23:36:09 -0500

    fixed JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ var mysql = require("mysql");
 
 var connection;
 // create connection with mysql
-if (process.env.JAWSDB_UBL){
-  connection = mysql.createConnection(process.env.JAWSDB_UBL);
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
